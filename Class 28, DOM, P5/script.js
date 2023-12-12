7ea3f5cd7b10c23e4c5c52c90f740b92eaa46d2e
hassanmehdi64 <hassanshafa64@gmail.com>
2023-12-11 23:07:50 -0800

    dom project 1 bg_changer

--- Class 28, DOM, P5/script.js
+++ Class 28, DOM, P5/script.js
@@ -4,14 +4,18 @@
         document.querySelector('.language').appendChild(li)
     }
     addLanguage("python")
-    addLanguage("typescript")
 
-    function addOptiLanguage(langName){
-        const li = document.createElement('li');
-        li.appendChild(document.createTextNode(langName))
-        document.querySelector('.language').appendChild(li)
-    }
-    addOptiLanguage('golang')
+    addLanguage("typescript")
+ 
+
+
+    // function addOptiLanguage(langName){
+    //     const li = document.createElement('li');
+    //     li.appendChild(document.createTextNode(langName))
+
+    //     document.querySelector('.language').appendChild(li)
+    // }
+    // addOptiLanguage('golang')
 
 
 
@@ -19,15 +23,20 @@
 
     //Edit
     const secondLang = document.querySelector("li:nth-child(2)")
-    console.log(secondLang);
-    //secondLang.innerHTML = "Mojo"
-    const newli = document.createElement('li')
-    newli.textContent = "Mojo"
-    secondLang.replaceWith(newli)
+    // console.log(secondLang);
+    // secondLang.innerHTML = "Ajmal"
+
+
+    // const newli = document.createElement('li')
+    // newli.textContent = "Naveed"
+    // secondLang.replaceWith(newli)
+
+
+
 
     //edit example 2
-    const firstLang = document.querySelector("li:first-child")
-    firstLang.outerHTML = '<li>TypeScript</li>'
+    // const firstLang = document.querySelector("li:first-child")
+    // firstLang.outerHTML = '<h2>TypeScript</h2>'
 
     //remove
     const lastLang = document.querySelector('li:last-child')
