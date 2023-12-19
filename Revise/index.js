4e01c5e3eed8bbbf07beaae9617e092b5913dccf
hassanmehdi64 <hassanshafa64@gmail.com>
2023-12-19 03:18:12 -0800

    Js Revise part 1

---
+++ Revise/index.js
@@ -0,0 +1,90 @@
+// let
+// Var
+// const
+
+const index = 'Hello Asheel'
+let indexlet = 'Hello Naveed'
+var indexvar = 'Hello Ajmal'
+
+// console.log(index);
+// console.log(indexlet);
+// console.log(indexvar);
+
+//const
+//const index = "nabe" // we cannot redeclare const
+// index = ' hello' // also we cannot resasign value '
+
+//let
+// let indexlet = 'kabeer'
+// indexlet = 'kabeer'
+// console.log(indexlet);
+
+// var
+// var indexvar = 'hell'
+//  console.log(indexvar);
+//  indexvar = ' Ajmal'
+//  console.log(indexvar);
+
+
+
+// -----------SCOPE------------
+// =====let scope=========
+// let a = 33
+// if (a == 33) {
+//     console.log(`number is ${a}`);
+    
+//     let b = 32
+    
+//     console.log(b);
+// }
+
+
+// =====var scope=========
+
+// var a = 33
+// if (a == 33) {
+    //     console.log(`number is ${a}`);
+    
+    //    var b = 32
+    
+    // }
+    // console.log(b);
+
+
+
+// =====const scope=========
+
+// const a = 33
+// if (a == 33) {
+//         console.log(`number is ${a}`);
+    
+//        const b = 32
+    
+//        console.log(b);
+//     }
+
+
+
+// ========================Operators===========================
+//= , ==, ===, +, -, <, >, !,*, **, %,
+
+// let a = 3
+// let b = 55
+
+// console.log( a % b);
+
+
+// =====================Logical Operators===========================
+// &&, ||, !
+
+let x = 33
+let y = 44
+
+
+if (x != 33 && y !=44 ) {
+    console.log('condtion is true');
+}else{
+    
+    console.log('condtion is false');
+
+}
