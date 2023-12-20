f9095d09004c3f3e3e63e599761909be85222365
hassanmehdi64 <hassanshafa64@gmail.com>
2023-12-20 03:10:58 -0800

    class 32 events p3

---
+++ Class 32, Events - P3/script.js
@@ -0,0 +1,19 @@
+
+// ================= Events Properties =======================
+
+// we can pass a parameter to access the properties using that object
+
+const eventprops = document.getElementById('box-4')
+eventprops.addEventListener('click', (e) => {
+    
+console.log(e);
+
+console.log(e.type);
+console.log(e.target);
+console.log(e.clientX, e.clientY);
+
+})
+
+
+
+
